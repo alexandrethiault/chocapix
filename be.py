054bbe4e74234a628b069296e1add91d2b0e3b78
Alexandre Thiault <58135519+alexandrethiault@users.noreply.github.com>
2019-11-29 20:01:04 +0100

    Update be.py

--- be.py
+++ be.py
@@ -15,16 +15,27 @@ else:
     import atexit
     from select import select
 
-after_series_number = 1.5 #temps donné pour coller le numéro de série (sec)
-after_quantity = 4 #et pour coller la quantité puis changer d'aliment
 
-appro = False # valeur par défaut, ne pas modifier ici
-archive = False # valeur par défaut, ne pas modifier ici
+time_1 = 1.25 # temps pour une quantité 1
+time_2 = 3 # temps pour une quantité 2
+time_more = 0.5 # temps supplémentaire pour chaque +1 en quantité
+time_max = 5 # temps max
+
+def paste_time(quantity):
+    if quantity == 1:
+        return time_1
+    elif quantity in [2, 3, 4, 5]:
+        return time_2 + (quantity-2) * time_more
+    else: # > 5 mais aussi nombres à virgule (légumes vendus au poids...)
+        return time_max
 
 brands = ["carrefour", "picard"]
 pasdappro = ["picard"]
 keyword = {"carrefour": "OOSHOP", "picard": "SIRET : 78493968805071"}
-finddate = {"picard": (lambda s: s[s.find("DATE : ")+7:s.find("DATE : ")+17]), "carrefour" : (lambda s: s[s.find("Date de commande : ")+19:s.find("Date de commande : ")+29])}
+finddate = {
+    "picard": (lambda s: s[s.find("DATE : ")+7:s.find("DATE : ")+17]),
+    "carrefour" : (lambda s: s[s.find("Date de commande : ")+19:s.find("Date de commande : ")+29])
+}
 
 ## Parsing
 
@@ -34,7 +45,7 @@ class Article:
         if brand == "carrefour":
             if len(string[0]) != 13:
                 raise ValueError
-            self.sernumber = int(string[0])
+            self.sernumber = string[0]
             self.name = " ".join(string[1:-3])
             self.qty = int(string[-3])
             stm2 = string[-2][string[-2].index('.')+3:]
@@ -45,7 +56,7 @@ class Article:
         elif brand == "picard":
             if len(string[0]) != 6:
                 raise ValueError
-            self.sernumber = int(string[0])
+            self.sernumber = string[0]
             if string[-1] == "OFFERT":
                 string[-1:] = ["0,00", "€", "0,00", "€", "0,00%"]
             if string[-2] != "€" or string[-4] != "€":
@@ -70,7 +81,7 @@ def stddate(jjmmaaaa):
     return f"{aaaa}{mm}{jj}"
 
 def get_from_file(filename):
-    raw = parser.from_file(filename)
+    raw = parser.from_file(filename,)
     string = raw['content']
     for br in brands:
         if keyword[br] in string:
@@ -124,53 +135,66 @@ def let_user_pause(timeout):
             input()
             preparation()
 
-def preparation():
+def preparation(qty = 0):
+    if qty == 1:
+        print("Les quantités sont maintenant toutes 1. Ca va aller plus vite.")
     print("Début dans ", end="", flush=True)
-    for i in range(6):
-        print(3-i//2 if i%2 == 0 else ".", end=' ', flush=True)
-        sleep(0.5)
+    for i in range(12):
+        print(3-i//4 if i%4 == 0 else ".", end='', flush=True)
+        sleep(0.25)
     print()
 
 def show_and_copy(article):
-    pyperclip.copy(str(article.sernumber))
-    print(str(article.sernumber), end='', flush=True)
-    let_user_pause(after_series_number)
-    pyperclip.copy(str(article.qty))
-    print(f" {article.qty}")
-    let_user_pause(after_quantity)
+    pyperclip.copy(article.sernumber)
+    print(f"{article.sernumber} - {article.qty}\t{article.name}")
+    let_user_pause(paste_time(article.qty))
 
 ## Lecture de la facture parsée, mise à jour des prix, compte-rendu
 
+def group_by_sernum(articles):
+    ans = {}
+    for article in articles:
+        if article.sernumber in ans:
+            ans[article.sernumber].qty += article.qty
+            ans[article.sernumber].qty = round(ans[article.sernumber].qty, 5)
+        else:
+            ans[article.sernumber] = article
+    return list(ans.values())
+
 def update_prices(parsedfile):
     date, brand, articles = parsedfile
+
     prices = {}
-    newarticlesindex = {}
-    newarticles = []
-    newprices = []
     try:
         with open(f"prix_{brand}.txt", 'r') as former:
             lines = [line.split(' ') for line in former.readlines()]
             for [key, value] in lines:
-                prices[int(key)] = float(value)
+                prices[key] = float(value)
     except:
         pass
-    if appro:
-        preparation()
 
+    appro_started = False
+    unique_started = False
+    newarticles = []
+    newprices = []
+    articles = group_by_sernum(articles)
+    articles.sort(key=lambda a:100 if a.qty%1 else a.qty, reverse=True)
     for article in articles:
+        if article.qty == 0:
+            break
         if article.sernumber in prices:
-            if article.sernumber not in newarticlesindex:
-                former_price = prices[article.sernumber]
-                if appro and article.qty > 0:
-                    show_and_copy(article)
-                if article.price != former_price:
-                    newprices.append((former_price, article))
-            else:
-                indx = newarticlesindex[article.sernumber]
-                newarticles[indx].qty += article.qty
-                newarticles[indx].qty = round(newarticles[indx].qty, 5)
+            former_price = prices[article.sernumber]
+            if appro:
+                if article.qty == 1 and not unique_started:
+                    unique_started = True
+                    preparation(1)
+                elif not appro_started:
+                    appro_started = True
+                    preparation()
+                show_and_copy(article)
+            if article.price != former_price:
+                newprices.append((former_price, article))
         else:
-            newarticlesindex[article.sernumber] = len(newarticles)
             newarticles.append(article)
         prices[article.sernumber] = article.price
 
@@ -199,7 +223,9 @@ def update_prices(parsedfile):
 
 if __name__ == "__main__":
     def main():
-        global appro, archive, after_series_number, after_quantity, kb
+        global appro, archive, time_1, time_2, time_more, time_max, kb
+        appro = False
+        archive = False
         entrypoint = update_prices
         files = []
         while len(sys.argv) > 1:
@@ -212,12 +238,16 @@ if __name__ == "__main__":
                 archive = True
             elif opt in ["pause_set", "set_pause"]:
                 try:
-                    after_series_number = float(sys.argv[2].replace(',', '.'))
+                    time_1 = float(sys.argv[2].replace(',', '.'))
                     sys.argv.pop(2)
-                    after_quantity = float(sys.argv[2].replace(',', '.'))
+                    time_2 = float(sys.argv[2].replace(',', '.'))
+                    sys.argv.pop(2)
+                    time_more = float(sys.argv[2].replace(',', '.'))
+                    sys.argv.pop(2)
+                    time_max = float(sys.argv[2].replace(',', '.'))
                     sys.argv.pop(2)
                 except:
-                    print(f"La commande {opt} demande 2 nombres après. Ces 2 nombres doivent être entiers ou à virgule.")
+                    print(f"La commande {opt} demande 4 nombres après. Ces 2 nombres doivent être entiers ou à virgule.")
                     sys.exit(1)
             else:
                 print(f"Option inconnue \"{sys.argv[1]}\".")
@@ -242,7 +272,7 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             print("\nScript interrompu définitivement")
         except UnboundLocalError:
-            print("\nMauvaise syntaxe. Exemples de syntaxe :\npython be.py 21.10_facture.pdf appro set_pause 2 5\npython be.py archive")
+            print("\nMauvaise syntaxe. Exemples de syntaxe :\npython be.py 21.10_facture.pdf appro set_pause 2 4 0.5 6\npython be.py archive")
         except FileNotFoundError:
             print("\nLa facture ou la base de données est introuvable. Il faut qu'elles soient dans le même dossier que le script. Vérifier le nom exact de la facture.")
         except (KeyError, NotImplementedError) as brand:
